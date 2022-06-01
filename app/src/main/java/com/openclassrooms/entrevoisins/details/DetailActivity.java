eb211bdc3962b0b90025a55b0f9cda623c364176
Pierre G <pr.gaillot@gmail.com>
2022-06-01 15:03:01 +0200

    delete some test

--- app/src/main/java/com/openclassrooms/entrevoisins/details/DetailActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/details/DetailActivity.java
@@ -1,15 +1,8 @@
 package com.openclassrooms.entrevoisins.details;
 
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import com.openclassrooms.entrevoisins.R;
 
 public class DetailActivity extends AppCompatActivity {
